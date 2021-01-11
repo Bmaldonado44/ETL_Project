817cdafe7554b3cf4b1855cbb841004f4ad553b0
Bmaldonado44 <70544086+Bmaldonado44@users.noreply.github.com>
2021-01-10 23:56:45 -0600

    3rd attempt

---
+++ schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Netlfix (
+  id INT PRIMARY KEY,
+	Years INT,
+  type_n TEXT,
+  title TEXT,
+	Area TEXT,
+	rating TEXT
+);
+
+
+CREATE TABLE Subscribers (
+  id INT PRIMARY KEY,
+  Subscribers INT,
+  Area TEXT,
+	Years INT
+);
